6d3fb159bea6fbeb0a57643de8998be30b49b74c
Nate Clark <natec425@gmail.com>
2019-01-18 21:08:42 -0600

    Super basics down with Previous and Next support

--- src/Carousel.js
+++ src/Carousel.js
@@ -1,10 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Carousel.css';
 
+const modifier = (childIndex, selectedIndex) => {
+    switch (childIndex - selectedIndex) {
+        case 0:
+            return 'center';
+        case -1:
+            return 'left';
+        case 1:
+            return 'right';
+        default:
+            return 'hidden';
+    }
+}
+
 const Carousel = props => {
+
+    const [selectedChild, setSelectedChild] = useState(0);
+
     return (
         <div className="carousel">
-            {props.children}
+            <div className="carousel__children-container">
+                {props.children.map((child, i) =>
+                    <div className={`carousel__child carousel__child--${modifier(i, selectedChild)}`} >
+                        {child}
+                    </div>)}
+            </div>
+            <div className="carousel__selectors-container">
+                <button onClick={() => setSelectedChild(selectedChild - 1)}>Previous</button>
+                <button onClick={() => setSelectedChild(selectedChild + 1)}>Next</button>
+            </div>
         </div>
     );
 }
