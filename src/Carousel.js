2f4c7bd867c88e5ba810b925a72669a1ba0dd9c3
Nate Clark <natec425@gmail.com>
2019-01-24 16:14:34 -0600

    It is dragging now

--- src/Carousel.js
+++ src/Carousel.js
@@ -35,18 +35,23 @@ const selectorModifiers = (selectorIndex, selectedIndex) => {
 const Carousel = props => {
 
     const [selectedChild, setSelectedChild] = useState(0);
+    const [drag, setDrag] = useState(null);
 
     return (
         <div className="carousel">
-            <div className="carousel__children-container">
+            <div className="carousel__children-container"
+                onTouchStart={evt => setDrag({ startX: evt.touches[0].pageX })}
+                onTouchMove={evt => setDrag(Object.assign({}, drag, { endX: evt.touches[0].pageX }))}
+                onTouchEnd={evt => { setDrag(null); console.log('down') }}
+                style={drag ? { transform: `translateX(${-(drag.startX - drag.endX)}px)` } : {}}>
                 {props.children.map((child, i) =>
-                    <div className={`carousel__child ${childModifiers(i, selectedChild)}`} >
+                    <div key={i} className={`carousel__child ${childModifiers(i, selectedChild)}`} >
                         {child}
                     </div>)}
             </div>
             <div className="carousel__selectors-container">
                 {props.children.map((_child, i) =>
-                    <button className={`carousel__selector ${selectorModifiers(i, selectedChild)}`} onClick={() => setSelectedChild(i)}></button>)}
+                    <button key={i} className={`carousel__selector ${selectorModifiers(i, selectedChild)}`} onClick={() => setSelectedChild(i)}></button>)}
             </div>
         </div >
     );
